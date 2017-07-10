5df8c88048e0786aeb4409463025bf83a0f90327
Petr Kalinin <petr@kalinin.nnov.ru>
2017-07-10 21:55:17 +0300

    Basic client browserifying

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,36 @@ var coffee = require('gulp-coffee');
 var babel = require('gulp-babel');
 var plumber = require('gulp-plumber');
 var server = require('gulp-develop-server');
+var coffeescript = require('coffeescript');
+var through = require('through');
 
-gulp.task('bundle', function() {
-  return browserify('client/App.js')
-    .transform('babelify', {presets: 'react'})
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('build/client/'));
+gulp.task('client:js', function() {
+    return browserify('./client/client.coffee')
+        .transform(function (file) {
+                var data = '';
+                return through(write, end);
+
+                function write (buf) { data += buf }
+                function end () {
+                    result = coffeescript.compile(data);
+                    this.queue(result);
+                    this.queue(null);
+                }
+        })
+        .transform('babelify', {presets: ['env'], extensions: [".coffee"]})
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./build/client/'));
 });
 
-gulp.task('bundleCoffee', function() {
+gulp.task('client:html', function() {
+  return gulp.src('client/**/*.html')
+    .pipe(gulp.dest('./build/client'));
+});
+
+gulp.task('client:bundle', ['client:js', 'client:html']);
+
+gulp.task('server:coffee', function() {
   return gulp.src('server/**/*.coffee')
     .pipe(sourcemaps.init())
     .pipe(plumber())
@@ -28,7 +48,7 @@ gulp.task('bundleCoffee', function() {
     .pipe(gulp.dest('./build/server'));
 });
 
-gulp.task('bundleJs', function() {
+gulp.task('server:js', function() {
   return gulp.src('server/**/*.js')
     .pipe(plumber())
     .pipe(babel({
@@ -37,14 +57,14 @@ gulp.task('bundleJs', function() {
     .pipe(gulp.dest('./build/server'));
 });
 
-gulp.task('bundleServer', ['bundleCoffee', 'bundleJs']);
+gulp.task('server:bundle', ['server:coffee', 'server:js']);
 
 gulp.task('watchServer', function() {
-    gulp.watch('server/**/*.coffee', ['bundleCoffee']);
-    gulp.watch('server/**/*.js', ['bundleJs']);
+    gulp.watch('server/**/*.coffee', ['server:coffee']);
+    gulp.watch('server/**/*.js', ['server:js']);
 });
 
-gulp.task( 'server:start', ['bundleServer'], function() {
+gulp.task( 'server:start', ['server:bundle'], function() {
     server.listen( { path: './build/server/server.js' } );
 });
 
@@ -52,4 +72,4 @@ gulp.task( 'server:restart', ['server:start'], function() {
     gulp.watch( [ './build/server/**/*' ], server.restart );
 });
 
-gulp.task('default', ['watchServer', 'server:restart']);
+gulp.task('default', ['client:bundle', 'watchServer', 'server:restart']);
--- server/server.coffee
+++ server/server.coffee
@@ -21,6 +21,9 @@ app.use(express.static('build/client'))
 app.get '/status', (req, res) -> 
   logger.info "Query string", req.query
   res.send "OK"
+  
+app.get '/', (req, res) ->
+    res.sendFile("./build/client/index.html")
 
 app.listen 3000, () ->
   logger.info 'Example app listening on port 3000!'
