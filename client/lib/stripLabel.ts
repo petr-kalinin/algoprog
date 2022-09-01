2f3e9f70ade7002834fb8990e64767fba406bfb1
Petr Kalinin <petr@kalinin.nnov.ru>
2022-09-01 21:13:37 +0300

    Fix stripLabel for undefined

--- client/lib/stripLabel.ts
+++ client/lib/stripLabel.ts
@@ -1,4 +1,7 @@
 export default function stripLabel(id: string): string {
+    if (!id) {
+        return id
+    }
     var idx = id.indexOf("!")
     if (idx != -1) {
         return id.substring(0, idx)
